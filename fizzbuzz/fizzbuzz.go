8fd6e2a8ae8c907305924745b30754b9b3a6d10a
Seth McCarthy <seth.mccarthy@olo.com>
2022-07-03 06:22:51 -0700

    Extract function refactor
    Handlily, vs code just did this for me.  Not the rename of the func
    afterwards though.

--- fizzbuzz/fizzbuzz.go
+++ fizzbuzz/fizzbuzz.go
@@ -7,15 +7,18 @@ import (
 
 func main() {
 	for i := 1; i <= 100; i++ {
-		output := strconv.FormatInt(int64(i), 10)
-		if i%15 == 0 {
-			output = "FizzBuzz"
-		} else if i%3 == 0 {
-			output = "Fizz"
-		} else if i%5 == 0 {
-			output = "Buzz"
-		}
-
-		fmt.Println(output)
+		fmt.Println(buildOutputString(i))
 	}
+}
+
+func buildOutputString(i int) string {
+	output := strconv.FormatInt(int64(i), 10)
+	if i%15 == 0 {
+		output = "FizzBuzz"
+	} else if i%3 == 0 {
+		output = "Fizz"
+	} else if i%5 == 0 {
+		output = "Buzz"
+	}
+	return output
 }
